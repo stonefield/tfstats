09ef9d8bba1b9856a5a03d6bf9a4f124a5a0b42e
Knut Stenmark <knut.stenmark@gmail.com>
2022-08-26 10:42:48 +0200

    Add support for versioning information in a project

--- lib/tfstats/collector.rb
+++ lib/tfstats/collector.rb
@@ -5,12 +5,23 @@ module Tfstats
   class Collector
     class << self
       def collect(directory, filespec, recursive, tabseparated)
-        statistics = process_dir(directory, filespec, recursive)
+        statistics = {}
+        process_dir(directory, filespec, recursive) do |collector, placeholder|
+          statistics[placeholder] = collector.collect
+        end
         puts output( statistics, tabseparated)
       end
 
+      def versions(directory, filespec, recursive, tabseparated)
+        versions = {}
+        process_dir(directory, filespec, recursive) do |collector, placeholder|
+          versions[placeholder] = collector.versions if collector.any? # Check versions only if there are any relevant files in the directory
+        end
+        puts output_versions( versions, tabseparated)
+      end
+
+
       def process_dir(directory, filespec, recursive)
-        statistics = {}
         basename = File.basename File.expand_path(directory)
         info "directory: #{directory}"
         info "basename: #{basename}"
@@ -20,12 +31,11 @@ module Tfstats
           Dir.glob("#{directory}/**/").each do |dir|
             info "traversing: #{dir}"
             placeholder = dir.sub(/^#{Regexp.escape(directory.to_s)}/, basename).chop
-            statistics[placeholder] = new(dir, filespec).collect
+            yield new(dir, filespec), placeholder
           end
         else
-          statistics[basename] = new(directory,filespec).collect
+          yield new(directory,filespec), basename
         end
-        statistics
       end
 
       def output(statistics, tabseparated)
@@ -51,6 +61,48 @@ module Tfstats
         str << separator
         str << (pattern % (["Total"] + sum(statistics).values_at(*fields) ))
         str << separator
+        str
+      end
+
+      def output_versions(statistics, tabseparated)
+        str = ''
+        info "statistics collected: #{statistics.inspect}"
+        if tabseparated
+          pattern = (["%s"] * 3).join("\t") + "\n"
+          separator = ''
+          str << (pattern % %w(Directory Provider Version))
+          statistics.each do |dir, info|
+            str << (pattern % [dir, 'terraform-version', info[:terraform_version]])
+            if info[:providers].is_a? Hash
+              info[:providers].each do |provider, version|
+                str << (pattern % [dir, provider, version])
+              end
+            else
+              str << (pattern % [dir, 'providers', info[:providers]])
+            end
+          end
+        else
+          max_name = statistics.map do |dir, info|
+            if info[:providers].is_a? Hash
+              info[:providers].map { |k,v| k.size }
+            else
+              info[:providers].size
+            end
+          end.flatten.max + 1
+          pattern = "    %-#{max_name}.#{max_name}s %8s\n"
+          statistics.each do |dir, info|
+            str << "#{dir}:\n"
+            str << "  terraform-version: #{info[:terraform_version]}\n"
+            if info[:providers].is_a? Hash
+              str << "  providers:\n"
+              info[:providers].each do |provider,version|
+                str << ( pattern % ["#{provider}:",version])
+              end
+            else
+              str << "  providers: #{info[:providers]}\n"
+            end
+          end
+        end
         str
       end
 
@@ -82,6 +134,11 @@ module Tfstats
       @directory, @filespec = directory, filespec
       @stats = {}
       @stats.default = 0
+    end
+
+    def any?
+      spec = File.join(directory, filespec)
+      Dir[spec].any?
     end
 
     def info(msg)
@@ -121,6 +178,64 @@ module Tfstats
 
       end
       @stats
+    end
+
+    def versions
+      @stats[:terraform_version] = terraform_version
+      @stats[:providers] = provider_versions
+      @stats
+    end
+
+    def terraform_version
+      file = File.join(directory, '.terraform-version')
+      version = ''
+      if File.exist? file
+        info "Fetching version from: #{file}"
+        version = File.read(file).chomp
+      else
+        info "No terraform version file found (#{file})"
+      end
+      version == '' ? 'undefined' : version
+    end
+
+    def provider_versions
+      file = File.join(directory, '.terraform.lock.hcl')
+      providers = {}
+      if File.exist? file
+        state = :root
+        provider = nil
+        begin
+          info "Fetching provider versions from: #{file}"
+          File.read(file).each_line.with_index do |line,i|
+            begin
+              case state
+              when :root
+                if line =~ /^\s*provider\s*\"(.+)\"\s*\{\s*$/
+                  provider = $1
+                  state = :provider
+                end
+              when :provider
+                if line =~ /^\s*version\s*=\s*"(.*)"\s*$/
+                  providers[provider] = $1
+                  state = :version
+                end
+              when :version
+                if line =~ /^\s*\}\s*$/
+                  provider = nil
+                  state = :root
+                end
+              else
+                raise ArgumentError, "File out of sync."
+              end
+            rescue ArgumentError => e
+              STDERR.puts "#{file}:#{i+1} #{e}"
+            end
+          end
+        rescue => e
+          STDERR.puts e
+        end
+      end
+      providers == {} ? 'undefined' : providers
     end
 
   end # Collector
--- spec/tfstats/collector_spec.rb
+++ spec/tfstats/collector_spec.rb
@@ -1,26 +1,82 @@
 RSpec.describe Tfstats::Collector do
 
-  describe '.collect' do
+  let(:modules_path) { fixture_file_path('modules')}
+  let(:filespec) { "*.tf" }
 
+  let(:statistics_output) do
+    {
+      "fixtures"=>{
+        :files=>2, :modules=>3, :lines=>51, :loc=>30, :resources=>2, :data=>4, :variables=>1
+        },
+      "fixtures/modules"=>{
+        :files=>3, :resources=>1, :lines=>46, :loc=>34, :modules=>1, :data=>1, :variables=>1
+        },
+      "fixtures/userdata"=>{}
+    }
+  end
+
+  let(:statistics) do
+    {
+      "fixtures"=>{
+        :data=>4, :files=>2, :lines=>51, :loc=>30, :modules=>3, :resources=>2, :variables=>1
+      }
+    }
+  end
+
+  let(:terraform_versions) do
+    {
+      "fixtures"=>{
+        :terraform_version => '1.0.9',
+        :providers => {
+          'registry.terraform.io/hashicorp/archive' => '2.2.0',
+          'registry.terraform.io/hashicorp/aws' => '3.67.0'
+        }
+      }
+    }
+  end
+
+
+  describe '.collect' do
+    subject { described_class.new(fixture_dir, filespec)}
+    specify do
+      allow(described_class).to receive(:new).and_return(subject)
+      expect(subject).to receive(:collect).and_return(statistics['fixtures'])
+      expect(described_class).to receive(:output).with(statistics, false)
+      described_class.collect(fixture_dir, filespec, false, false)
+    end
+  end
+
+  describe '.versions' do
+    subject { described_class.new(fixture_dir, filespec)}
+    specify do
+      allow(described_class).to receive(:new).and_return(subject)
+      expect(subject).to receive(:versions).and_return(terraform_versions['fixtures'])
+      expect(described_class).to receive(:output_versions).with(terraform_versions, false)
+      described_class.versions(fixture_dir, filespec, false, false)
+    end
+
+    it 'should output versions correctly' do
+      expect { described_class.versions(fixture_dir, filespec, false, false)}.to output(yaml_formatted_version_info).to_stdout
+    end
   end
 
   describe '.process_dirs' do
-    subject { described_class.process_dir(fixture_dir, "*.tf", recursive) }
+    subject { described_class }
     context 'single dir' do
-      let(:recursive) { false }
-      let(:statistics) do
-        {
-          "fixtures"=>{
-            :data=>4, :files=>2, :lines=>51, :loc=>30, :modules=>3, :resources=>2, :variables=>1
-          }
-        }
+      let(:expected_yield) { [:object, 'fixtures'] }
+      specify do
+        allow(subject).to receive(:new).and_return(:object)
+        expect { |b| subject.process_dir(fixture_dir, filespec, false, &b)}.to yield_with_args(*expected_yield)
       end
-      specify { expect(subject).to eq statistics }
     end
     context 'recursively' do
-      let(:recursive) { true }
-      let(:statistics) { statistics_output }
-      specify { expect(subject).to eq statistics }
+      let(:expected_yields) {
+        [[:object, 'fixtures'], [:object, 'fixtures/modules'], [:object, "fixtures/userdata"]]
+      }
+      specify do
+        allow(subject).to receive(:new).and_return(:object)
+        expect { |b| subject.process_dir(fixture_dir, filespec, true, &b)}.to yield_successive_args(*expected_yields)
+      end
     end
   end
 
@@ -64,11 +120,82 @@ RSpec.describe Tfstats::Collector do
     end
   end
 
+  let(:yaml_formatted_version_info) do
+    <<~TEXT
+      fixtures:
+        terraform-version: 1.0.9
+        providers:
+          registry.terraform.io/hashicorp/archive:    2.2.0
+          registry.terraform.io/hashicorp/aws:       3.67.0
+    TEXT
+  end
+
+  describe '.version_output' do
+    subject { described_class.output_versions(terraform_versions, tabseparated) }
+    let(:no_terraform_versions) do
+      {
+        "fixtures"=>{
+          :terraform_version => 'undefined',
+          :providers => 'undefined'
+        }
+      }
+    end
+    context 'with yaml-format' do
+      let(:tabseparated) { false }
+      context 'with versioning info' do
+        it 'should be yaml' do
+          expect(subject).to eq yaml_formatted_version_info
+        end
+      end
+      context 'without versioning info' do
+        let(:terraform_versions) { no_terraform_versions }
+        let(:yaml_formatted_without_versions) do
+          <<~TEXT
+            fixtures:
+              terraform-version: undefined
+              providers: undefined
+          TEXT
+        end
+        it 'should be yaml' do
+          expect(subject).to eq yaml_formatted_without_versions
+        end
+      end
+    end
+    context 'with tab-format' do
+      let(:tabseparated) { true }
+      context 'with versioning info' do
+        let(:like_a_file) do
+          <<~TEXT
+            Directory\tProvider\tVersion
+            fixtures\tterraform-version\t1.0.9
+            fixtures\tregistry.terraform.io/hashicorp/archive\t2.2.0
+            fixtures\tregistry.terraform.io/hashicorp/aws\t3.67.0
+          TEXT
+        end
+        it 'should be like a file' do
+          expect(subject).to eq like_a_file
+        end
+      end
+      context 'without versioning info' do
+        let(:terraform_versions) { no_terraform_versions }
+        let(:tab_formatted_without_versions) do
+          <<~TEXT
+            Directory\tProvider\tVersion
+            fixtures\tterraform-version\tundefined
+            fixtures\tproviders\tundefined
+          TEXT
+        end
+        it 'should be yaml' do
+          expect(subject).to eq tab_formatted_without_versions
+        end
+      end
+    end
+  end
+
   describe '#collect' do
     subject { described_class.new(modules_path, filespec) }
 
     context 'with default filespec' do
-      let(:filespec) { "*.tf" }
       specify do
         as_defined = { :data=>1, :files=>3, :lines=>46, :loc=>34, :modules=>1, :resources=>1, :variables=>1 }
         expect(subject.collect).to eq as_defined
@@ -84,18 +211,19 @@ RSpec.describe Tfstats::Collector do
     end
   end
 
-  let(:modules_path) { fixture_file_path('modules')}
-
-  let(:statistics_output) do
-    {
-      "fixtures"=>{
-        :files=>2, :modules=>3, :lines=>51, :loc=>30, :resources=>2, :data=>4, :variables=>1
-        },
-      "fixtures/modules"=>{
-        :files=>3, :resources=>1, :lines=>46, :loc=>34, :modules=>1, :data=>1, :variables=>1
-        },
-      "fixtures/userdata"=>{}
-    }
+  describe '#terraform_version' do
+    subject { described_class.new(modules_path, filespec) }
+    context 'when file does not exist' do
+      specify do
+        expect(subject.terraform_version).to eq 'undefined'
+      end
+    end
+    context 'when file exist' do
+      let(:modules_path) { fixture_dir }
+      specify do
+        expect(subject.terraform_version).to eq '1.0.9'
+      end
+    end
   end
 
 end
